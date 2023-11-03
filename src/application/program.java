a546632f15449cd8cfb54e7a4f249e626455fd58
pedromgt <pedromigot@gmail.com>
2023-11-03 08:56:09 -0300

    first class

--- src/application/program.java
+++ src/application/program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.position;
+
 public class program {
 
 	public static void main(String[] args) {
 		
-		System.out.print("hello word");
+		position pos = new position (3,5);
+		System.out.println(pos);
 	}
 
 }
